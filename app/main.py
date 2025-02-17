cbd5153ea110ce283f58b0e26a39e37436e1fa95
Alexandr <oleksandrglushen2005@gmail.com>
2025-02-17 23:16:15 +0200

    Finished work

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": 18,
-        "column": 80,
-        "message": "line too long (99 > 79 characters)",
-        "name": "E501",
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
         "source": "flake8",
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "line": errors["line_number"],
-        "column": errors["column_number"],
-        "message": errors["text"],
-        "name": errors["code"],
-        "source": "flake8"
+        "errors":[format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_linter_report(linter_report: list) -> list:
+    return [format_single_linter_file(path, errors) for path, errors in linter_report.items()]
+
+
