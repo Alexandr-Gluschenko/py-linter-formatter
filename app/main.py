9df2e16af825262008e922d16ddd88fb4e60ef43
Alexandr <oleksandrglushen2005@gmail.com>
2025-02-17 22:29:38 +0200

    converting errors into the required format

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": 18,
+        "column": 80,
+        "message": "line too long (99 > 79 characters)",
+        "name": "E501",
+        "source": "flake8",
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": errors["line_number"],
+        "column": errors["column_number"],
+        "message": errors["text"],
+        "name": errors["code"],
+        "source": "flake8"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
